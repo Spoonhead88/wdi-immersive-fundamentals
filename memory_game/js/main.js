80ac9fac2ac2c3b10a2bf166a5692907f759c36e
Spoonhead88 <spoonhead06@hotmail.com>
2019-07-24 13:43:50 +0100

    created logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,6 +24,32 @@ var cards = [
 
 var cardsInPlay = [];
 
+var shuffleOption = document.getElementById('shuffle');
+var instructionsOption = document.getElementById('instructions');
+var InstroSection = document.querySelector('section');
+var endMsg = document.querySelector('h3');
+
+function shuffle(array) {
+  var currentIndex = array.length;
+  var temporaryValue;
+  var randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element... at random
+    randomIndex = Math.floor(Math.random() * currentIndex); // chooses random index
+    currentIndex -= 1; //once a random index is chosen, -1  count until they have all been swapped
+
+    // And swap it with the current element. 
+    temporaryValue = array[currentIndex]; // temporary value stores the value that is about to be swapped
+    array[currentIndex] = array[randomIndex]; // this line swaps the value at currentindex with the one that was chosen at random
+    array[randomIndex] = temporaryValue; // this line puts the original value at currentdiex into the random index
+  }
+
+  return array;
+}
+
 function createBoard(){
 	for(i = 0; i < cards.length; i++){
 		var cardElement = document.createElement('img');
@@ -34,26 +60,43 @@ function createBoard(){
 	}
 }
 
+function clearBoard(){
+	for(i = 0; i < cards.length; i++){
+		var image = document.querySelector('#game-board > img');
+		image.parentNode.removeChild(image);
+	}
+}
+
+function reset(){
+	//clear the array and then reset
+	for(i = cardsInPlay.length; i > 0; i--){
+		cardsInPlay.pop();
+	}
+	clearBoard();
+	shuffle(cards);
+	createBoard();
+}
+
 function checkForMatch(){
 	if(cardsInPlay[0] === cardsInPlay[1])
 		{
-			console.log("You found a match. ");
+			endMsg.textContent = "You Won!";
+			endMsg.classList.toggle("visible");
+			console.log('should have won');
+			console.log(endMsg.textContent);
 		}
 		else
 		{
-			console.log("Sorry try again. ");
+			endMsg.textContent = "Wrong! Shuffle to try again.";
+			endMsg.classList.toggle("visible");
+			console.log('should have lost');
 		}
 }
 
 function flipCard(){
 	cardId = this.getAttribute('data-id');
 
-	console.log("User flipped " + cards[cardId].rank);
-
 	cardsInPlay.push(cards[cardId].rank);
-
-	console.log(cards[cardId].cardImage);
-	console.log(cards[cardId].suit);
 
 	this.setAttribute('src',cards[cardId].cardImage);
 
@@ -63,4 +106,12 @@ function flipCard(){
 	}
 }
 
+function toggleVisible(){
+	InstroSection.classList.toggle("visible");
+}
+
+shuffleOption.addEventListener('click', reset);
+instructionsOption.addEventListener('click', toggleVisible);
+
+shuffle(cards);
 createBoard();
